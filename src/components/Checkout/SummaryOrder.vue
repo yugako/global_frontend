bdd978e8021585e85c6f5edcb55282cfbc9ec7ad
Yurii Koltso <yurkoga25@gmail.com>
2019-02-09 09:35:27 +0200

    Fixed summary order styles

--- src/components/Checkout/SummaryOrder.vue
+++ src/components/Checkout/SummaryOrder.vue
@@ -49,51 +49,5 @@
 	};
 </script>
 <style lang="sass">
-	.table
-		border-bottom: 1px solid #ededed
-		border-collapse: collapse
-		border-spacing: 0
-		font-size: 14px
-		line-height: 2
-		margin: 0 0 10px
-		width: 100%
-		background-color: #f1f2f3
-		thead
-			th
-				border-bottom: 2px solid #000
-		td
-			font-size: 13px
-			padding: 20px 10px
-			text-align: center
-			vertical-align: middle
-			border-left: 1px solid #eaeaea
-			border-bottom: 1px solid #eaeaea
-			border-right: 1px solid #eaeaea
-		th
-			font-weight: bold
-			padding: 20px 10px
-			text-align: center
-			text-transform: uppercase
-			vertical-align: middle
-			white-space: nowrap
-			border-left: 1px solid #eaeaea
-			border-right: 1px solid #eaeaea
-			color: #777
-			font-size: 14px
-		&-footer
-			border-top: 2px solid #000
-		.title-top
-			border-bottom: 2px solid #000
-			&_item
-				color: #000
-		.product
-			&-name
-			    margin-left: 10px
-			    color: #606060
-			    font-size: 14px
-			&-total
-				&__head, &__price
-					color: #000
-					font-size: 14px
-					font-weight: bold
+	@import '../../assets/sass/summaryOrder'
 </style>
