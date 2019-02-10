76399cbdc7781398386c9c855d2d9c3b9523c9e7
Yurii Koltso <yurkoga25@gmail.com>
2019-02-10 10:02:56 +0200

    Add new dish to store

---
+++ src/components/Admin/NewItem.vue
@@ -0,0 +1,88 @@
+<template>
+	<div class="wrapper">
+		<Banner text='Add new dish' class='new-banner'/>
+		<div class="container">
+				<div class="row">
+					<div class="col-lg-8 offset-lg-2">
+						<form class="menu-item__form">
+						    <div class="row">
+						        <div class="col-md-12 col-12">
+						        	<label for='name'>
+						        		Dish name:	
+						        	</label>                                 
+						            <input v-model='newItem.name' type="text" id="name" placeholder="Name">
+						        </div>
+						        <div class="col-12 col-md-6">
+						        	<label for="price">Dish price, $</label>
+						            <input v-model='newItem.price' placeholder="Price" id="price" type="text">
+						        </div>
+						        <div class="col-12 col-md-6">
+						        	<label for="weight">Weight, g</label>
+						            <input v-model='newItem.weight' placeholder="Weight" type="text">
+						        </div>
+						        <div class="col-12">
+						         	<!-- <label for="img">Path to image</label>                              
+						            <input v-model='newItem.img' id="img" type="text" placeholder="Path to image"> -->
+						            <PreviewImage @getImg='getSrc' />
+						        </div> 
+						        <div class="col-12">
+						        	<label for="excerpt">Short description</label>
+						            <textarea v-model='newItem.excerpt' class="excerpt" maxlength="100"  placeholder="Short description" type="text"></textarea>
+						        </div>
+						        <div class="col-12">
+						        <label for="include">Ingradients</label>                              
+						            <input v-model='newItem.include' id="include" type="text" placeholder="Components">
+						        </div> 
+						        <div class="col-md-12 col-12">
+						        	<label for="descr">Dish description</label>                             
+						            <textarea v-model='newItem.descr' class="descr" maxlength="500" id="descr" placeholder="Description"></textarea>
+						        </div>                       
+						    </div>
+						</form>
+						<div @click='addToMenu(newItem)' class="menu-item__save">
+							<button class="button">Save</button>
+						</div>
+						<!-- /.menu-item__save -->
+					</div>
+				</div>
+		</div>
+	</div>
+	<!-- /.wrapper -->
+	
+</template>
+<script>
+	import PreviewImage from "@/components/Admin/PreviewImage.vue";
+	import Banner from "@/components/Banner.vue";
+
+	export default {
+		data () {
+			return {
+				imageSrc: '',
+				newItem: {
+					name: '',
+					price: 0,
+					img: this.imageSrc || require('@/assets/img/menu-list/placeImg.jpg'),
+					weight: 0,
+					quantity: 1,
+					excerpt: '',
+					include: '',
+					descr: ''
+				}
+				
+			}
+		},
+		methods: {
+			addToMenu (item) {
+				this.$store.commit('addToMenu', {item});
+			},
+			getSrc (data) {
+				console.log(data);
+		        this.imageSrc = data;
+		    },
+		},
+		components: {
+			PreviewImage,
+			Banner
+		}
+	}
+</script>
