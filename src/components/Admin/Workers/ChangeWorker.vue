b4e46bf76b674332a55743819936da3ca9337364
Yurii Koltso <yurkoga25@gmail.com>
2019-02-10 21:19:55 +0200

    Edit worker's data

---
+++ src/components/Admin/Workers/ChangeWorker.vue
@@ -0,0 +1,50 @@
+<template>
+	<div class="wrapper">
+		<Banner class='home-banner' text='Change dish'/>
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-8 offset-lg-2">
+					<div class="menu-item" v-for='(worker,index) in workers' :key='index'>
+						<div v-if='worker.id === proId'>
+							<div class="menu-item__title">
+								Edit worker's data: {{worker.name}}
+							</div>
+							<!-- /.menu-item__title -->
+							<EditWorker :worker='worker' />
+							
+							<!-- /.menu-item__save -->
+						</div>
+					</div>
+					<!-- /.menu-item -->
+				</div>
+				<!-- /.col-lg-8 offset-lg-4 -->
+			</div>
+			<!-- /.row -->
+			
+		</div>
+		<!-- /.container -->
+	</div>
+	<!-- /.wrapper -->
+	
+</template>
+<script>
+	import Banner from "@/components/Banner.vue";
+	import EditWorker from "@/components/Admin/Workers/EditWorker.vue";
+	export default {
+		data () {
+			return {
+				proId: this.$route.params.id,
+				workers: this.$store.state.workers,
+				
+			}
+		},
+		
+		components: {
+			Banner,
+			EditWorker
+		}
+	}
+</script>
+<style lang="sass">
+	@import "../../../assets/sass/menuChange"
+</style>
---
+++ src/components/Admin/Workers/EditWorker.vue
@@ -0,0 +1,59 @@
+<template>
+	<div class="container">
+		<form @submit.prevent='saveItem(worker)' class="menu-item__form">
+		    <div class="row">
+		        <div class="col-md-12 col-12">
+		        	<label for='name'>
+		        		Worker name:	
+		        	</label>                                 
+		            <input required v-model='worker.name' type="text" id="name" placeholder="Name">
+		        </div>
+		        <div class="col-12 col-md-12">
+		        	<label for="password">Worker password</label>
+		            <input required placeholder="Password" id="password" v-model='worker.password' type="text">
+		        </div>                    
+		    </div>
+		    <div v-if='save' class="success">Saved!</div>
+		    <div v-if='error' class="error">Please fill all fields!</div>
+		    <div class="menu-item__save">
+		    	<button type="submit" class="button">Save</button>
+		    </div>
+		</form>
+	</div>
+	
+</template>
+<script>
+	export default {
+		props: ['worker'],
+		data () {
+			return {
+				save: false,
+				error: false,
+			}
+		},
+		methods: {
+			saveItem (item) {
+				if(this.validationValue(item)) {
+					this.save = true;
+					setTimeout(() => {
+						this.save = false;
+					}, 1500);
+				} else {
+					this.error = true;
+					setTimeout(() => {
+						this.error = false;
+					}, 1500);
+				}
+		       
+		    },
+		    validationValue(values) {
+		    	let result = [];
+		    	for (let key in values) {
+		    		result.push(values[key]);
+		    	}
+
+		    	return result.every(item => item);
+		    }
+		},
+	}
+</script>
