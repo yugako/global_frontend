094fc44d7b1fd370cbd7576df7d25ae080e53010
Yurii Koltso <yurkoga25@gmail.com>
2019-02-18 12:56:40 +0200

    Register user in system

--- src/components/Signin/Register.vue
+++ src/components/Signin/Register.vue
@@ -1,19 +1,38 @@
 <template>
-	<form action="#">
+	<form @submit.prevent="onSubmit">
 	    <div class="single-input">
-	        <input class="cr-round--lg" type="text" placeholder="User name">
+	        <input class="cr-round--lg" v-model.trim="register.name" type="text" placeholder="User name">
 	    </div>
 	    <div class="single-input">
-	        <input class="cr-round--lg" type="email" placeholder="Email address">
-	    </div>
-	    <div class="single-input">
-	        <input class="cr-round--lg" type="password" placeholder="Password">
-	    </div>
-	    <div class="single-input">
-	        <input class="cr-round--lg" type="password" placeholder="Confirm password">
+	        <input class="cr-round--lg" v-model.trim="register.password" type="password" placeholder="Password">
 	    </div>
 	    <div class="single-input">
 	        <button type="submit" class="food__btn"><span>Sign Up</span></button>
 	    </div>
 	</form>
 </template>
+<script>
+	import axios from 'axios';
+	export default {
+		name: 'register',
+		data () {
+			return {
+				register: {},
+				errors: []
+			}
+		},
+		methods: {
+		    onSubmit () {
+		      	axios.post(`http://localhost:3000/register/`, this.register)
+		      	.then(response => {
+		        	alert("Registered successfully")
+		        	this.$store.commit('showLogin');
+		      	})
+		      	.catch(e => {
+		        	console.log(e)
+		        	this.errors.push(e)
+		      	})
+		    },
+		}
+	}
+</script>
