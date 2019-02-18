7eb6da1f2a136609b95f12c5904c6efc02134462
Yurii Koltso <yurkoga25@gmail.com>
2019-02-18 12:56:06 +0200

    Logged user in system

--- src/components/Signin/LogIn.vue
+++ src/components/Signin/LogIn.vue
@@ -1,13 +1,42 @@
 <template>
-	<form action="#">
+	<form @submit.prevent="onSubmit">
 	    <div class="single-input">
-	        <input type="text" placeholder="User name or email">
+	        <input type="text" v-model.trim='login.name' placeholder="User name">
 	    </div>
 	    <div class="single-input">
-	        <input type="password" placeholder="Password">
+	        <input type="password" v-model.trim="login.password" placeholder="Password">
 	    </div>
 	    <div class="single-input">
 	        <button type="submit" class="food__btn"><span>Go</span></button>
 	    </div>
 	</form>
 </template>
+<script>
+	import axios from "axios";
+	export default {
+		name: 'login',
+		data () {
+			return {
+				login:{},
+				errors: []
+			}
+		},
+		methods: {
+			onSubmit() {
+				axios.post('http://localhost:3000/login/', this.login)
+					.then(response => {
+						localStorage.setItem('jwtToken', response.data.token)
+						this.$store.commit('hideForm');
+						this.$store.dispatch("getOrders");
+					})
+					.catch(e => {
+						console.log(e)
+						this.errors.push(e)
+					})
+			},
+			register() {
+				this.$store.commit('showRegister');
+			}
+		}
+	}
+</script>
