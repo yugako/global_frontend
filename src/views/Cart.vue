75d4fe0195d5c0c144df76da75d0f2b1fc65d542
Yurii Koltso <yurkoga25@gmail.com>
2019-02-19 21:08:26 +0200

    Fix cart output

--- src/views/Cart.vue
+++ src/views/Cart.vue
@@ -4,50 +4,47 @@
 		<!-- /.cart-header -->
 		<div class="container">
 		    <div class="row">
-		        <div class="col-md-12 col-sm-12 ol-lg-12">
-		            <form action="#">               
-		                <div class="table-content table-responsive">
-		                    <table>
-		                        <thead>
-		                            <tr class="title-top">
-		                                <th class="product-thumbnail">Image</th>
-		                                <th class="product-name">Product</th>
-		                                <th class="product-price">Price</th>
-		                                <th class="product-quantity">Quantity</th>
-		                                <th class="product-subtotal">Total</th>
-		                                <th class="product-remove">Remove</th>
-		                            </tr>
-		                        </thead>
-		                        <tbody>
-		                        	
-		                            <tr v-for='(item, index) in cart' :key='index'>
-		                                <td class="product-thumbnail">
-		                                	<a href="#">
-		                                		<img src="@/assets/img/menu-list/placeImg.jpg" :alt="item.title" />
-		                                	</a>
-		                                </td>
-		                                <td class="product-name">
-		                                	<a href="#">{{item.title}}</a>
-		                                </td>
-		                                <td class="product-price">
-		                                	<span class="amount">{{item.price}}$</span>
-		                                </td>
-		                                <td class="product-quantity">
-		                                	<input min="0" type="number" v-model='item.quantity' />
-		                                </td>
-		                                <td class="product-subtotal">{{countTotal(index)}}</td>
-		                                <td class="product-remove" @click='removeFromCart(index)'>
-		                                	<span class="button">Remove</span>
-		                                </td>
-		                            </tr>
-		                        </tbody>
-		                    </table>
-		                    <div v-if='cart.length === 0'>
-		                        <span>No items in cart</span>
-		                    </div>
-		                </div>
-		            </form> 
-		         
+		        <div class="col-md-12 col-sm-12 ol-lg-12">	                           
+	                <div class="table-content table-responsive">
+	                    <table>
+	                        <thead>
+	                            <tr class="title-top">
+	                                <th class="product-thumbnail">Image</th>
+	                                <th class="product-name">Product</th>
+	                                <th class="product-price">Price</th>
+	                                <th class="product-quantity">Quantity</th>
+	                                <th class="product-subtotal">Total</th>
+	                                <th class="product-remove">Remove</th>
+	                            </tr>
+	                        </thead>
+	                        <tbody>
+	                        	
+	                            <tr v-for='(item, index) in cart' :key='index'>
+	                                <td class="product-thumbnail">
+	                                	<a href="#">
+	                                		<img src="@/assets/img/menu-list/placeImg.jpg" :alt="item.title" />
+	                                	</a>
+	                                </td>
+	                                <td class="product-name">
+	                                	<a href="#">{{item.title}}</a>
+	                                </td>
+	                                <td class="product-price">
+	                                	<span class="amount">{{item.price}}$</span>
+	                                </td>
+	                                <td class="product-quantity">
+	                                	<input min="0" type="number" v-model='item.quantity' />
+	                                </td>
+	                                <td class="product-subtotal">{{countTotal(index)}}</td>
+	                                <td class="product-remove" @click='removeFromCart(index)'>
+	                                	<span class="button">Remove</span>
+	                                </td>
+	                            </tr>
+	                            <tr v-if='cart.length === 0'>
+	                            	<td class='empty' colspan="6">No items in cart</td>
+	                            </tr>
+	                        </tbody>
+	                    </table>
+	                </div>
 		        </div>
 		    </div>
 		    <div class="row">
@@ -58,7 +55,7 @@
 		                    <span>{{totalPrice}}</span>
 		                </div>
 		                <div class="cartbox__order">
-		                	<router-link to='/payment'>Take order</router-link>
+		                	<router-link :class='{disabled: isEmpty}' to='/payment'>Take order</router-link>
 		                </div>
 		                <!-- /.cartbox__order -->
 		            </div>
@@ -90,7 +87,10 @@ export default {
   computed: {
   	totalPrice () {
   		return this.$store.getters.countTotalOrder;
-  	},	
+  	},
+  	isEmpty () {
+  		return this.cart.length === 0 ? true : false;
+  	}	
   },
   components: {
     Banner
