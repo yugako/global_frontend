489101cd6fe6b7c7fa38b2170c24deb42cb97c76
Yurii Koltso <yurkoga25@gmail.com>
2019-02-19 15:53:40 +0200

    Remove some inputs

--- src/components/Checkout/BillingForm.vue
+++ src/components/Checkout/BillingForm.vue
@@ -4,43 +4,22 @@
 		<transition name="slide-fade">
 		<form action="#" v-show='show' class="billing-form">
 		    <div class="row">
-		        <div class="col-12">
-		            <select>
-		              <option value="1">Select a country</option>
-		              <option value="2">bangladesh</option>
-		              <option value="3">Algeria</option>
-		              <option value="4">Afghanistan</option>
-		              <option value="5">Ghana</option>
-		              <option value="6">Albania</option>
-		              <option value="7">Bahrain</option>
-		              <option value="8">Colombia</option>
-		              <option value="9">Dominican Republic</option>
-		            </select>
-		        </div>
 		        <div class="col-md-6 col-12">                                 
 		            <input type="text" placeholder="First Name">
 		        </div>
 		        <div class="col-md-6 col-12">                             
 		            <input type="text" placeholder="Last Name">
 		        </div>
-		        <div class="col-12">                              
-		            <input type="text" placeholder="Company Name">
-		        </div>
+		        
 		        <div class="col-12">
 		            <input placeholder="Street address" type="text">
 		        </div>
-		        <div class="col-12">
-		            <input placeholder="Apartment, suite, unit etc. (optional)" type="text">
-		        </div>
+		        
 		        <div class="col-12">
 		            <input placeholder="Town / City" type="text">
 		        </div>
-		        <div class="col-md-6 col-12">                                 
-		            <input type="text" placeholder="State / County">
-		        </div>
-		        <div class="col-md-6 col-12">                                 
-		            <input placeholder="Postcode / Zip" type="text">
-		        </div>
+		        
+		        
 		        <div class="col-md-6 col-12">                                 
 		            <input type="email" placeholder="Email Address">
 		        </div>
