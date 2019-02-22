e4082ec5ecbcdf24e29c89cd3cbf2e053f940791
Yurii Koltso <yurkoga25@gmail.com>
2019-02-22 11:06:10 +0200

    Added menuTable and workersTable component

--- src/views/Admin.vue
+++ src/views/Admin.vue
@@ -4,95 +4,15 @@
 		<div class="container">
 			<div class="admin-content">
 				<div class="admin-content__workers">
-					<h1 @click='showWorkers = !showWorkers' class="admin-content__title">Workers List</h1>
+					<h1 @click='toggleWorkersList' class="admin-content__title">Workers List</h1>
 					<!-- /.admin-content__title -->
-					<transition name="slide-fade">
-						<div v-if='showWorkers' class="wrapper">
-							<div class="table-content table-responsive">
-							    <table>
-							        <thead>
-							            <tr class="title-top">
-							                <th>Name</th>
-							                <th>Username</th>
-							                <th>Password</th>
-							             	<th>Actions</th>
-							            </tr>
-							        </thead>
-							        <tbody>
-							            <tr v-for='(worker,index) in workers' :key='worker._id'>
-							                <td class="worker-name">
-							                	{{worker.name}}
-							                </td>
-							                <td class="worker-name">
-							                	{{worker.username}}
-							                </td>
-							                <td class="worker-password">
-							                	{{worker.password}}
-							                </td>
-							                <td class="actions">
-							           			<span @click='goToEditWorker(worker._id)' class="actions-button button">Edit</span>
-
-							                	<span @click='removeFromWorkers(worker._id)' class="actions-button button">Remove</span>
-							                </td>
-							            </tr>
-							            
-							        </tbody>
-							    </table>
-							</div>
-							<div class="admin-content__add">
-								<button @click='addNewWorker' class="admin-content__add_button">Add worker</button>
-							</div>
-							<!-- /.admin-content__add -->
-						</div>
-						<!-- /.wrapper -->
-					</transition>
+					<WorkersTable />
 				</div>
 				<!-- /.admin-content__workers -->
 				<div class="admin-content__dishes">
-					<h1 @click='showDishes = !showDishes' class="admin-content__title">Menu List</h1>
+					<h1 @click='toggleDishesList' class="admin-content__title">Menu List</h1>
 					<!-- /.admin-content__title -->
-					<transition name="slide-fade">
-						<div v-if='showDishes' class="wrapper">
-							<div class="table-content table-responsive">
-							    <table>
-							        <thead>
-							            <tr class="title-top">
-							                <th class="product-thumbnail">Image</th>
-							                <th class="product-name">Name</th>
-							                <th class="product-price">Price</th>
-							                <th class="product-descr">Short description</th>
-							                <th class="product-remove">Actions</th>
-							            </tr>
-							        </thead>
-							        <tbody>
-							            <tr v-for='(dish, index) in dishesList' :key='dish._id'>
-							                <td class="product-thumbnail">
-							                	<img :src="dish.img" alt="product img" />
-							                </td>
-							                <td class="product-name">
-							                	{{dish.title}}
-							                </td>
-							                
-							                <td class="product-price">
-							                	<span class="amount">{{dish.price}}$</span>
-							                </td>
-							               
-							                <td class="product-excerpt">{{dish.excerpt}}</td>
-							                <td class="actions">
-							           			<span class="actions-button edit" @click='goToEditDish(dish._id)'>Edit</span>
-							                	<span @click='removeFromStore(dish._id)' class="actions-button remove">Remove</span>
-							                </td>
-							            </tr>
-							        </tbody>
-							    </table>
-							</div>
-							<div class="admin-content__add">
-								<button @click='addNewItem' class="admin-content__add_button">Add dish</button>
-							</div>
-							<!-- /.admin-content__add -->
-						</div>
-						<!-- /.wrapper -->
-					</transition>
+					<MenuTable />
 				</div>
 				<!-- /.admin-content__dishes -->
 			</div>
@@ -103,79 +23,23 @@
 	</section>
 </template>
 <script>
-	import axios from 'axios';
-	import Banner from "@/components/Banner.vue";
-	
+	import Banner from "@/components/Banner";
+	import MenuTable from "@/components/Admin/Menu/MenuTable";
+	import WorkersTable from "@/components/Admin/Workers/WorkersTable";
+
 	export default {
-		created () {
-			if(this.$store.getters.logged) {
-				axios.get('http://localhost:3000/users')
-				.then(response => {
-					this.workers = response.data;
-				})
-				.catch(e => {
-					console.log(e);
-					if (e.response.status === 401) {
-						this.$router.push({
-						    name: 'home'
-						});
-						this.$store.commit('showForm');
-						this.$store.commit('showLogin');
-					}
-				})
-			} else {
-				this.$router.push({
-				    name: 'home'
-				});
-				this.$store.commit('showForm');
-				this.$store.commit('showLogin');
-			}
-			
-		},
-		// beforeRouteLeave(to, from, next) {
-		// 	if(!this.$store.state.login.logged) {
-		// 		next();
-		// 	} else {
-		// 		next(false);
-		// 	}
-			
-		// },
-		beforeCreate: function() {
-		  this.$options.computed = {
-		     dishesList(){
-			     return this.$store.getters.Dishes
-			 },
-		  }
-		},
-		data () {
-			return {
-				showWorkers: false,
-				showDishes: true,
-				workers: []
-			}
-		},
 		methods: {
-			goToEditDish(dishId) {
-			    this.$router.push({name:'change',params:{id:dishId}})
+			toggleDishesList() {
+				this.$store.commit('toggleDishesList');
 			},
-			goToEditWorker(id) {
-			    this.$router.push({name:'edit',params:{id:id}})
-			},
-			addNewItem() {
-				this.$router.push({ name: 'add'}) 
-			},
-			addNewWorker() {
-				this.$router.push({ name: 'add_worker'}) 
-			},
-			removeFromStore (index) {
-				this.$store.dispatch('deleteDishes', index)
-			},
-			removeFromWorkers (index) {
-			  	this.$store.dispatch('deleteWorkers', index)
-			},
+			toggleWorkersList() {
+				this.$store.commit('toggleWorkersList');
+			}
 		},
 		components: {
 			Banner,
+			MenuTable,
+			WorkersTable
 		}
 	}
 </script>
