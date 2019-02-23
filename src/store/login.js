ec8f706ab9dd475ab4c6f7166735284ce8051d47
Yurii Koltso <yurkoga25@gmail.com>
2019-02-23 12:45:53 +0200

    Saved current user name in local storage

--- src/store/login.js
+++ src/store/login.js
@@ -1,6 +1,7 @@
 export default {
 	state: {
 		logged: JSON.parse(localStorage.getItem('log')),
+		authUser: JSON.parse(localStorage.getItem('logUser')),
 	},
 	mutations: {
 		isLogIn (state) {
@@ -11,10 +12,17 @@ export default {
 			state.logged = false;
 			localStorage.setItem('log', JSON.stringify(state.logged))
 		},
+		setUserName (state, payload) {
+			state.authUser = payload;
+			localStorage.setItem('logUser', JSON.stringify(state.authUser))
+		}
 	},
 	getters: {
 		logged: state => {
 			return state.logged;
 		},
+		UserName: state => {
+			return state.authUser;
+		}
 	}
 }
