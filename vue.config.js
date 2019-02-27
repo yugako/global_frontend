68ba38035207e5e72fc7484dee2e3e42960a5007
Yurii Koltso <yurkoga25@gmail.com>
2019-02-27 17:10:01 +0200

    Use env vars

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,9 @@
+var webpack = require('webpack');
+
 module.exports = {
   devServer: {
-    proxy: 'http://localhost:3000'
+    proxy: process.env.VUE_APP_API_URL,
+    port: process.env.PORT || 4563,
+    host: process.env.HOST || '127.0.0.1',
   }
 }
