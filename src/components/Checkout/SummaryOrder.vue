7ad19d138035fc4ec35c1a3fca867e7cd1b0f669
Yurii Koltso <yurkoga25@gmail.com>
2019-03-04 20:59:40 +0200

    Check all fields before order placing

--- src/components/Checkout/SummaryOrder.vue
+++ src/components/Checkout/SummaryOrder.vue
@@ -22,7 +22,8 @@
 					</tr>
 				</tbody>
 			</table>
-			<button @click='addToQueue()' :disabled='this.$store.state.cart.cart.length === 0' class="submit">Place order</button>
+			<div v-if='disabled'>You must entered all fields!</div>
+			<button @click='addToQueue()' class="submit">Place order</button>
 		</div>
 		<!-- /.table-responsive -->
 	</div>
@@ -35,6 +36,7 @@
 	  	return {
 	  		cart: this.$store.state.cart.cart,
 	  		number: localStorage.getItem('number') || 1,
+	  		disabled: false
 	  	}
 	  },
 	  methods: {
@@ -43,24 +45,36 @@
 	  	},
 	  	addToQueue () {
 	  		let cart = this.cart;
+	  		if (this.validationFields('billing') && this.validationFields('pay')) {
 
-	  		cart.forEach((elem, index) => {
-		  		this.$store.dispatch('saveOrder', {
-		 			title: elem.title,
-		 			number: this.countOrder,
-			    	price: this.countTotal(index),
-			    	amount: elem.quantity,
-			    	action: 'Take in order',
-			    	status: 'unprocessed'
-				});
-	  		});
-	  		this.number++;
-	  		localStorage.setItem('number', this.number);
-			setTimeout(() => {
-				this.$store.commit('cleanCart', cart);
-			}, 0);
+	  			cart.forEach((elem, index) => {
+			  		this.$store.dispatch('saveOrder', {
+			 			title: elem.title,
+			 			number: this.countOrder,
+				    	price: this.countTotal(index),
+				    	amount: elem.quantity,
+				    	action: 'Take in order',
+				    	status: 'unprocessed'
+					});
+		  		});
+		  		this.number++;
+		  		localStorage.setItem('number', this.number);
+				setTimeout(() => {
+					this.$store.commit('cleanCart', cart);
+				}, 0);
+	  		} else {
+	  			this.disabled = false;
+	  		}
+	  		
 				
 		},
+		validationFields(name) {
+	  		let elems = [...document.forms[name].elements];
+	  		let result = elems.every( (elem) => {
+	  			return elem.value;
+	  		});
+	  		return result;
+	  	}
 	  },
 	  computed: {
 	  	countTotalOrder () {
